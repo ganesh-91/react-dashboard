a585e6131d5bf37762c55980865b51d598273394
ganesh-91 <ganesh.khaire91@gmail.com>
2017-09-01 00:44:11 +0530

    drag and drop poc added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+
+import Header from "./components/header/header";
 
 class App extends Component {
   render() {
@@ -10,11 +13,37 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <Header />
+        {/* <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
       </div>
     );
+  }
+  allowDrop(ev) {
+    ev.preventDefault();
+  }
+
+  drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+
+  drop(ev) {
+    ev.preventDefault();
+
+    var data = ev.dataTransfer.getData("text");
+    var ref = this.refs[data];
+    debugger;
+    var el = {
+      tagName: ref.tagName.toLowerCase(),
+      type: ref.type,
+      className: ref.className,
+      id: ref.id
+    };
+    var elReact = React.createElement(el.tagName, el);
+    this.setState({ obj: this.state.obj.concat(elReact) });
+    // ReactDOM.render(elReact, document.getElementById("app1"));
+    this.state.obj;
   }
 }
 
